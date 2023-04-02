4829f8c309ff98df04337ea65eef7288d78419e0
JAVED HASAN <hasanjaved155@gmail.com>
2023-04-02 13:23:10 +0530

    CODING BLOCK

--- src/lec35_Inheritance/Client.java
+++ src/lec35_Inheritance/Client.java
@@ -3,10 +3,12 @@ package lec35_Inheritance;
 public class Client {
     public static void main(String[] args) {
         // Case 1
-        P obj = new P();
-        System.out.println(obj.d);
-        System.out.println(obj.d1);
-        obj.fun();
-        obj.fun1();
+//        P obj = new P();
+//        System.out.println(obj.d);
+//        System.out.println(obj.d1);
+//        obj.fun();
+//        obj.fun1();
+        // Case 2
+        P obj = new C();
     }
 }
